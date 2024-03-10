6974c55f04dd95bfa37cb29e5b46a7633a269e0c
Jasper Oh <ohyj0906@gmail.com>
2024-03-10 06:42:42 -0700

    if not authorize session user, redirect to home

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,10 +1,27 @@
 "use client"
-import React from "react";
+import React , {useEffect} from "react";
+import {redirect, useRouter} from "next/navigation"
 import { useSession } from "next-auth/react";
 
 export default function HomeLayout({ children }) {
 
+  const { data: session, status } = useSession();
+  const {push} = useRouter();
+
+  useEffect(() => {
+    if (status !== "loading") {
+      if (session){
+        push("/domain")
+      }else{
+        redirect("/")
+      }
+      
+    }
+  },[session]);
+
+
   return (
+
     <div style={{
       backgroundImage: "url('/night_firewatch.png')",
       backgroundSize: "100% 100%"
