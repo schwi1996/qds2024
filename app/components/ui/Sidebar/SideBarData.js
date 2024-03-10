414b9fe5ea2e7276231c6fe43a9034e071a6904c
Sungmok Cho <chosm7489@gmail.com>
2024-03-10 01:22:21 -0800

    fixed sidebar conflicts

--- app/components/ui/Sidebar/SideBarData.js
+++ app/components/ui/Sidebar/SideBarData.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { AiFillHome } from 'react-icons/ai';
-import { IoIosAddCircle } from 'react-icons/io';
-import { MdJoinFull } from 'react-icons/md';
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { IoIosAddCircle } from "react-icons/io";
+import { MdJoinFull } from "react-icons/md";
+import "../../../(site)/create/page";
 
 const SidebarData = [
   {
-    title: 'Home',
-    path: '../../../domain/page.jsx',
+    title: "Home",
+    path: "../../../page",
     icon: <AiFillHome />,
-    cName: 'nav-text',
+    cName: "nav-text",
   },
   {
-    title: 'Create Room',
-    path: '../../../create/page.jsx',
+    title: "Create Room",
+    path: "../../../(site)/create/page",
     icon: <IoIosAddCircle />,
-    cName: 'nav-text',
+    cName: "nav-text",
   },
   {
-    title: 'Join Room',
-    path: '../../../join/page.jsx',
+    title: "Join Room",
+    path: "../../../(site)/join/page",
     icon: <MdJoinFull />,
-    cName: 'nav-text',
+    cName: "nav-text",
   },
 ];
 
